124a0666825e62eca7f1dd730305b0a6f574fa05
Alex Su <alexanderytsu@gmail.com>
2023-10-11 16:35:28 +1100

    update to builtin actors that uses implicit execution for cron jobs

--- builtin/tests/builtin_actors_tests.rs
+++ builtin/tests/builtin_actors_tests.rs
@@ -1,8 +1,35 @@
-use fil_actors_integration_tests::tests::withdraw_balance_success_test;
+use fil_actors_integration_tests::tests::TEST_REGISTRY;
+use fil_actors_integration_tests::tests::*;
+use fvm_workbench_api::analysis::TraceAnalysis;
 use fvm_workbench_builtin_actors::setup;
 
 #[test]
 fn withdraw_balance_test() {
     let w = setup();
     withdraw_balance_success_test(&w);
+}
+
+#[test]
+fn benchmark_builtin_actors() {
+    for (test_name, test_fn) in TEST_REGISTRY.lock().unwrap().iter() {
+        let w = setup();
+        println!(
+            "\n\n\n========================================================================\n\n\n"
+        );
+        println!("Running test: {}", test_name);
+        test_fn(&w);
+        let trace = w.peek_execution_trace().unwrap();
+        println!("{}", trace.format());
+        let analysis = TraceAnalysis::build(trace.clone());
+        println!("{}", analysis.format_spans());
+    }
+}
+
+// do not commit: run the problematic test here to isolate it's failure
+#[test]
+fn problematic_test() {
+    let w = &setup();
+    //    =============
+    aggregate_bad_sector_number_test(w);
+    // ============
 }
