7a585c6f189f489291f6a0b87dd9dee7ec89988a
Alex Su <alexanderytsu@gmail.com>
2023-10-26 17:00:21 +1100

    run test registry

--- builtin/tests/builtin_actors_tests.rs
+++ builtin/tests/builtin_actors_tests.rs
@@ -1,5 +1,6 @@
-use fil_actors_integration_tests::tests::change_owner_success_test;
-use fil_actors_integration_tests::tests::withdraw_balance_success_test;
+use fil_actors_integration_tests::tests::*;
+use fil_actors_integration_tests::TEST_REGISTRY;
+use fvm_workbench_api::analysis::TraceAnalysis;
 use fvm_workbench_builtin_actors::setup;
 
 #[test]
@@ -13,4 +14,32 @@ fn withdraw_balance_success() {
 fn change_owner_success() {
     let w = setup();
     change_owner_success_test(&w);
+}
+
+#[test]
+fn account_authenticate_message() {
+    let w = setup();
+    account_authenticate_message_test(&w);
+}
+
+#[test]
+fn test_registry() {
+    let registry = TEST_REGISTRY.lock().unwrap();
+    for (name, (speed, test)) in registry.iter() {
+        println!("{}: ({})", name, speed);
+        if speed < &1 {
+            let w = setup();
+            test(&w);
+            let maybe_trace = w.peek_execution_trace();
+            if let Some(trace) = maybe_trace {
+                println!("{}", trace.format());
+                let analysis = TraceAnalysis::build(trace.clone());
+                println!("{}", analysis.format_spans());
+            }
+            println!("===============================================\n");
+        } else {
+            println!("skipping");
+            println!("===============================================\n");
+        }
+    }
 }
